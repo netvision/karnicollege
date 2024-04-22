dc26fe315c90fa1e111da40c425552e3f017e61a
unknown <rakesh@jangid.co.in>
2024-04-22 13:41:07 +0530

    debugged

--- src/pages/society.vue
+++ src/pages/society.vue
@@ -4,7 +4,7 @@ import society from '@/assets/societyreg.jpg'
 <template>
     <AppHeader />
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 mb-12">
-        <h2 class="text-2xl p-2 px-8 border-b-2">Management Committee</h2>
+        <h2 class="text-2xl p-2 px-8 border-b-2">Society Registration Certificate</h2>
         <div class="p-6 w-full">
           <img :src="society" />
         </div>
