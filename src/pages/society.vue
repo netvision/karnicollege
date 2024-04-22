00c2cfaf17a5766f373c5e9bfc91fdb6f8bc9c64
unknown <rakesh@jangid.co.in>
2024-04-22 13:40:23 +0530

    debugged

--- src/pages/society.vue
+++ src/pages/society.vue
@@ -1,9 +1,12 @@
+<script setup>
+import society from '@/assets/societyreg.jpg'
+</script>
 <template>
     <AppHeader />
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 mb-12">
         <h2 class="text-2xl p-2 px-8 border-b-2">Management Committee</h2>
         <div class="p-6 w-full">
-          <img src="society-registration.jpg" />
+          <img :src="society" />
         </div>
     </div>
     <Footer />
